c590f31f608005861cfdb22c2a2b6667856c0a96
serraleone <serraleones@gmail.com>
2021-12-05 14:39:37 +0900

    'reacttest'

--- ReactProject/wpf-app/src/index.js
+++ ReactProject/wpf-app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App1 from './App1';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
